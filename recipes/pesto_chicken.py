72c201e809ca7b517e2916d50d44dcf77bbb96f2
Ryan McClue <re.mcclue@protonmail.com>
2024-03-22 16:24:07 +1100

    Pesto chicken

---
+++ recipes/pesto_chicken.py
@@ -0,0 +1,36 @@
+from .common import *
+
+recipe = [
+  Ingredient("chicken-breast", 750, INGREDIENT_CAT.MEAT),
+  Ingredient("fettucine-pasta", 500, INGREDIENT_CAT.OTHER),
+  Ingredient("onion-powder", 2, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("garlic-powder", 2, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("pine-nuts", 30, INGREDIENT_CAT.OTHER),
+  Ingredient("garlic", 3, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("lemon", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-basil", 60, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("parmesan", 30, INGREDIENT_CAT.DAIRY),
+  Ingredient("capsicum", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("squash", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("zuchinni", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("red-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("penne-pasta", 500, INGREDIENT_CAT.OTHER),
+]
+
+'''
+## Method
+    1. Trim chicken breasts. Coat with onion powder, garlic powder, salt, pepper and 1tbsp of olive oil.
+    2. Sear chicken for about 2 minutes each side.
+    3. Put chicken in oven at 200degrees for 13 minutes
+       Take out and let chicken rest in pan.
+    4. Setting oven to grill/broil setting, toast 30g of pinenuts for 1minute-1minute30
+    5. Take leaves off basil, reserve stems of 1 basil bunch and coursely chop.
+       Blend in 3 garlic cloves, 1/2 juice of lemon, 30g parmesan, pine nuts
+       Once mixed in, add basil, salt, pepper and 1/2cup olive oil.
+    6. Take chicken out of pan and slice against grain (i.e. vertically) into thin slices.
+       Tear slices and place into bowl.
+    7. Cook capsicum and red onion until fragrant. Add zuchinni and squash until soft.
+       Add to bowl
+    7. Add pesto sauce to bowl and mix.
+    8. Serve with olive oil, parmesan and pepper.
+'''

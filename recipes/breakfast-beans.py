34831bbd1338902b7d93b079db0c9118e6a622b6
Ryan McClue <re.mcclue@protonmail.com>
2024-04-24 18:00:52 +1000

    U

---
+++ recipes/breakfast-beans.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("cannellini-beans", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("tin-tomatoes", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("dark-brown-sugar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("egg", 4, INGREDIENT_CAT.DAIRY),
+  Ingredient("avocado", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("red-wine-vinegar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("tomato-paste", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("brown-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-rosemary", 0.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("fresh-chives", 0.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("smoked-paprika", 0.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("chilli-flakes", 0.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("thyme", 0.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("beef-chipolatas", 1, INGREDIENT_CAT.MEAT),
+]
+
+'''
+1. Soften onion with salt and pepper.
+2. Add sliced garlic.
+3. Add 2tbsp sugar, 2tbsp vinegar.
+4. Add 1/2tsp smoked paprika, 1/2tsp thyme, 1/2tsp chilli flakes.
+5. Roast 2tbsp tomato paste.
+6. Add 2 tins tomatoes 
+7. Add 2 tins of cannellini beans, salt, pepper and sprig of fresh rosemary
+8. Simmer for 25minutes
+9. On 200c, bake chipolatas for 16minutes.
+'''

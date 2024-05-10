46414d3eff075f562275ee15f5f2edd79d13f63a
Ryan McClue <re.mcclue@protonmail.com>
2024-05-10 12:26:08 +1000

    Schnitzel

---
+++ recipes/chicken_schnitzel.py
@@ -0,0 +1,36 @@
+from .common import *
+
+recipe = [
+  Ingredient("chicken-breast", 700, INGREDIENT_CAT.MEAT),
+  Ingredient("panko-breadcrumbs", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("lemon", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-dill", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-parsley", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("potato", 1000, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("salad-mix", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("creme-friache", 1, INGREDIENT_CAT.DAIRY),
+  Ingredient("egg", 3, INGREDIENT_CAT.DAIRY),
+  Ingredient("whole-gherkins", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("flour", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("dijon-mustard", 1, INGREDIENT_CAT.OTHER),
+]
+
+'''
+1. Whisk 3 eggs with 1tbsp soy sauce.
+   Add 4tbsp flour to another plate.
+   Add 4tbsp breadcrumbs to another plate.
+2. Butterfly chicken.
+   Flatten with rolling pin between 2 sheets of baking paper.
+2. Season both sides of chicken.
+   Coat chicken in flour, then eggs, then breadcrumbs.
+   Once on breadcrumbs, roll chicken between baking paper again to maintain.
+   Cook on medium heat with 2cm of oil, flipping as needed roughly 2-3 minutes each side.
+3. Slice potatoes in half and cook.
+   Add butter, salt, pepper and mix.
+   Add parsley.
+4. Cut 2 large gherkins into batons.
+   Mix 2tsp dijon mustard and 2tbsp creme fraiche in bowl.
+   Stir in gherkin and chopped dill with splash of pickling liquid if needed.
+5. Serve with salad and lemon wedge on chicken.
+'''

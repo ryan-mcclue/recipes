4250684473d51f876c9e98f37bb5e31729227f9b
Ryan McClue <re.mcclue@protonmail.com>
2024-05-25 19:32:01 +1000

    cottage

--- recipes/cottage_pie.py
+++ recipes/cottage_pie.py
@@ -2,28 +2,58 @@ from .common import *
 
 recipe = [
   Ingredient("brown-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("shallot", 1, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("celery", 2, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("carrot", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("celery", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("carrot", 1, INGREDIENT_CAT.FRUIT_VEG),
   Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("parsley", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("thyme", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("rosemary", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("potato", 1000, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("beef-mince", 1000, INGREDIENT_CAT.MEAT),
   Ingredient("frozen-peas", 1, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("green-beans", 200, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("beef-mince", 800, INGREDIENT_CAT.MEAT),
-  Ingredient("worcestershire-sauce", 1, INGREDIENT_CAT.OTHER),
-  Ingredient("beef-stockpot", 1, INGREDIENT_CAT.OTHER),
-  Ingredient("potato", 1500, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("worcestershire-sauce", 2, INGREDIENT_CAT.OTHER),
   Ingredient("tomato-paste", 2, INGREDIENT_CAT.OTHER),
-  Ingredient("parsley", 1, INGREDIENT_CAT.CONDIMENT),
-  Ingredient("rosemary", 1, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("red-wine", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("beef-stock", 1, INGREDIENT_CAT.OTHER),
 ]
 
 '''
+Mashed Potato - 
+ 
+1kg (2.2lbs) - Kestrel, Russet or Yukon Gold Potatoes, Peeled & Halved
+100g - Unsalted Butter, Softened
+150ml - Full Fat Milk, Warmed
+2 - Free Range Egg Yolks
+4 Tbsp (30g) - Parmigiana Cheese (Parmesan)
+ 
+Cottage Pie - 
+ 
+2 Tbsp - Olive Oil
+1kg (2.2lb) - Beef Mince
+1 - Carrot, Peeled & Grated
+1 - Brown Onion, Grated
+1 - Large Celery Rib, Grated
+3 - Cloves of Garlic, Grated
+3g - Flat Leaf Parsley, Finely Chopped 
+3g - Rosemary Leaves, Finely Chopped (optional)
+3 - Sprigs of Thyme, Leaves Only, Finely Chopped
+2 Tbsp (50g) - Concentrated Tomato Paste
+2 Tbsp (30ml) - Worcestershire Sauce
+1 Cup (250ml) - Red Wine (Can be substituted for 1 Cup (250ml) Vegetable or Chicken Stock
+1 Cup (250ml) - Vegetable or Chicken Stock
+1 Cup - Frozen Peas  
+Seasoning to taste 
+ 
 ## Instructions
-  1. Mash potatoes with milk and butter. Add salt, pepper, parsley, rosemary.
-  2. Brown mince with salt, pepper and rosemary. Set aside.
-  3. Cook garlic, onion, shallot until fragrant.
-  4. Add in carrot, celery. Season with salt and pepper. Add frozen peas.
-  5. Add back mince. Add tomato paste and worcester sauce.
-  6. Add in beef stock pot. Simmer for 5minutes
-  7. Line with potato and bake on 200C for 20minutes 
+  1. Grate onion, carrot and celery. 
+     Dice parsely, thyme and rosemary.
+  2. Brown mince with salt, pepper
+     Add grated veges, herbs, minced garlic, tomato paste and worcester sauce
+  3. Add red wine and reduce
+  4. Add beef stock and cook for 15minutes.
+     Add peas.
+  5. Mash potatoes with milk and butter. Add 2 egg yolks
+     Top on mix with some salt and pepper.
+     Score with fork
+  6. Bake for 25minutes at 180deg.
 '''

1bb812ee7e3caba8a5823ee6aaa2e3d0e9c1275d
Ryan McClue <re.mcclue@protonmail.com>
2024-05-27 14:31:21 +1000

    laksa

---
+++ recipes/laksa.py
@@ -0,0 +1,42 @@
+from .common import *
+
+recipe = [
+  Ingredient("thai-red-curry-paste", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("curry-powder", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("boneless-skinless-chicken-thigh", 800, INGREDIENT_CAT.MEAT),
+  Ingredient("coconut-milk", 400, INGREDIENT_CAT.OTHER),
+  Ingredient("chicken-stock", 1000, INGREDIENT_CAT.OTHER),
+  Ingredient("tofu-puffs", 12, INGREDIENT_CAT.OTHER),
+  Ingredient("fish-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("vermicilli-noodles", 800, INGREDIENT_CAT.OTHER),
+  Ingredient("bean-sprouts", 200, INGREDIENT_CAT.FRUIT_VEG),
+] 
+'''
+1. Heat oil. Stir in curry paste until fragrant.
+   Stir curry powder for a bit.
+   Add chicken and stir fry.
+   Add coconut milk.
+   Add chicken stock.
+   Simmer for 5 minutes.
+2. Add tofu puffs and simmer until cooked.
+3. Add fish sauce, sugar, and salt
+4. To serve, scoop out protein onto noodles. 
+   Then add broth.
+   Then add bean sprouts
+
+Ingredients
+2 tbsp vegetable oil
+2 tbsp Thai red curry paste
+2 tbsp curry powder
+200g (7 oz) sliced chicken thigh fillets
+400ml (13.5 fl oz) can coconut milk
+4 cups chicken stock
+400g (14 oz) peeled prawns (optional)
+200g (7 oz) Asian fish balls (optional)
+12 fried tofu puffs
+2 tbsp fish sauce
+1 tsp sugar
+800g (1.7 lb) cooked noodles, to serve
+Bean shoots, boiled egg and coriander (cilantro) leaves, to serve
+'''

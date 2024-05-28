78e08fc55c511f2965b7b3748e66d111b2c30cf1
Ryan McClue <re.mcclue@protonmail.com>
2024-05-28 14:35:40 +1000

    update ramen

--- recipes/ramen.py
+++ recipes/ramen.py
@@ -1,107 +1,60 @@
 from .common import *
 
 recipe = [
-  Ingredient("thai-red-curry-paste", 2, INGREDIENT_CAT.OTHER),
-  Ingredient("curry-powder", 2, INGREDIENT_CAT.OTHER),
-  Ingredient("boneless-skinless-chicken-thigh", 800, INGREDIENT_CAT.MEAT),
-  Ingredient("coconut-milk", 400, INGREDIENT_CAT.OTHER),
-  Ingredient("chicken-stock", 1000, INGREDIENT_CAT.OTHER),
-  Ingredient("tofu-puffs", 12, INGREDIENT_CAT.OTHER),
-  Ingredient("fish-sauce", 2, INGREDIENT_CAT.OTHER),
-  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
-  Ingredient("vermicilli-noodles", 800, INGREDIENT_CAT.OTHER),
-  Ingredient("bean-sprouts", 200, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("chicken-stock", 1500, INGREDIENT_CAT.OTHER),
+  Ingredient("unsweetened-soy-milk", 500, INGREDIENT_CAT.OTHER),
+  Ingredient("instant-ramen-noodles", 4, INGREDIENT_CAT.OTHER),
+  Ingredient("bok-choy", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("chicken-mince", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("ginger", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("spring-onion", 6, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("gochujang", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 3, INGREDIENT_CAT.OTHER),
+  Ingredient("crispy-chilli-oil", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("white-vinegar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("asian-sesame-paste", 1, INGREDIENT_CAT.OTHER),
 ] 
 '''
-1. Boil eggs by swirling to have yolk in centre 
-
-heat chicken stock. add soy milk. bring to boil
-
-mince ginger and garlic.
-slice spring onion.
-half bok choy
-
-stir fry chicken mince (leaving on one side to caramelise a bit)
-add ginger, garlic, spring onion and gochujang. mix
-add some soy sauce
-
-mix sesame paste, crispy chilli oil, vinegar and soy sauce in a bowl.
-place a dollop of this in bowl before serving
-
-add 2minute noodle packets to water. 
-add bok choy when noodles almost done
-place noodles onto serving bowl. 
-then bok choy
-add some broth mix. mix in paste from bottom of bowl
-add more broth
-
+1. Simmer chicken stock and soy milk while doing other steps.
+2. Mince ginger and garlic. Slice spring onion.
+   Stir fry chicken mince till evenly coloured (leaving on one side to caramelise a bit)
+   Add ginger, garlic, spring onion and gochujang. Mix
+   Drizzle soy sauce.
+   Reduce heat and simmer for 3-4minutes.
+3. Mix sesame paste, crispy chilli oil, vinegar and soy sauce in a bowl.
+   Distribute dollops in serving bowls
+4. Cook noodles. Add to serving bowls
+5. Half bok choy.
+   In noodle water, cook bok choy for about 1minute until tender
+   Add bok choy to serving bowls
+6. Ladle some broth to serving bowl. 
+   Mix in paste from bottom of bowl.
+   Add more broth
+7. Top with chicken mince and extra spring onion.
+(Boil eggs by swirling to have yolk in centre)
 
 Ingredients
 
 6 cups chicken stock
-
 2 cups unsweetened soy milk
-
 4 x individual instant ramen noodle cakes
-
 4 baby bok choy, halved lengthways
-
 2 soft-boiled eggs, halved, to serve 
-
 finely sliced green chilli, to serve
-
  
-
 Spicy chicken topping
-
 1 tbsp vegetable oil
-
 300g (10.5 oz) chicken mince
-
 3 garlic cloves, roughly chopped
-
 1 tsp finely grated ginger
-
 2 tbsp spring onion, finely chopped, plus extra to serve
-
 2 tbsp gochujang
-
 1 tbsp soy sauce
-
  
-
 Soup flavouring paste
-
 ¼ cup Japanese white sesame paste* or smooth peanut butter
-
 2 tbsp chilli oil
-
 2 tbsp white vinegar
-
 2 tbsp soy sauce
-Steps
-
-    Step 1
-
-    Bring a pot of water to the boil (you’ll need it to cook the noodles and bok choy later).
-    Step 2
-
-    For the broth, add the chicken stock and soy milk to a large pot and place over high heat. Bring to a simmer.
-    Step 3
-
-    Meanwhile, to make the spicy chicken topping, place a wok or frying pan over high heat. Once hot, swirl in the oil. Add the chicken mince, spread it out in the wok, and then leave to sear for 2 minutes until it’s evenly coloured. Add in the garlic, ginger and spring onion, followed by the gochujang paste. Stir-fry for another minute until well incorporated into the other ingredients and everything is smelling delicious. Drizzle in the soy sauce. Turn the heat to low and gently simmer for 3-4 minutes or until most of the sauce has been absorbed and the chicken is cooked through. Remove from the heat and set aside until ready to serve.
-    Step 4
-
-    For the soup flavouring paste, combine the ingredients in a small bowl. Divide among serving bowls.
-    Step 5
-
-    Cook the ramen noodles in the boiling water according to packet instructions. Use tongs to remove the noodles and divide among serving bowls. Use the same noodle cooking water to quickly cook the bok choy for about a minute or until just tender. Transfer the bok choy into the serving bowls.
-    Step 6
-
-    Ladle over the hot soup, mixing to combine it with the paste. Top with the spicy chicken mixture and an egg half, sprinkle with chilli and extra spring onion, then serve.
-    Notes
-    Notes:
-
-    – Japanese or Chinese white sesame paste is made from roasted sesame seeds. It’s available online or from Asian supermarkets. Substitute peanut butter if you can’t find the sesame paste.
-
 '''

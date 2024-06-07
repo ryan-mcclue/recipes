2e380a60207dcb2e73a4695f837beb1428abf951
Ryan McClue <re.mcclue@protonmail.com>
2024-06-07 10:51:51 +1000

    nacho

---
+++ recipes/vegetarian_nachos.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: zlib-acknowledgement
+
+from .common import *
+
+recipe = [
+  Ingredient("red-onion", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("chilli", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-tomatoes", 400, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-coriander", 30, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("avocado", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("cheddar-cheese", 120, INGREDIENT_CAT.DAIRY),
+  Ingredient("lime", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("cumin", 2, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("paprika", 2, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("coriander", 2, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("kidney-beans", 400, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("chicken-stock", 250, INGREDIENT_CAT.OTHER),
+  Ingredient("tortilla-chips", 200, INGREDIENT_CAT.OTHER),
+]
+
+'''
+1. Dice onion, mince garlic. Half to separate bowls.
+2. Dice chilli and add half to bowl with all paprika, cumin and coriander. 
+3. Fry half onions and garlic. 
+   Add half chilli and all spices.
+   Add half chopped tomatoes and all kidney beans. 
+   Add stock and simmer for 20minutes.
+4. Quarter and deseed half tomatoes and dice.
+   Add remaining garlic, onion, chilli and half coriander leaves into bowl
+   Add 2tbsp olive olive and zest and juice of 1/2 lime.
+   Add salt and pepper.
+5. Mash avocado and some lime juice and coriander leaves.
+6. Layer chips with cheese and put in 180deg oven for 3-4minutes.
+   Then add beans, salsa and avocado
+
+Ingredients - 
+    2 small red onions
+    2 cloves of garlic
+    1 red chilli
+    4 tbsp of olive oil
+    400g large fresh tomatoes
+    2 tsp paprika
+    2 tsp ground cumin
+    2 tsp ground coriander
+    400g tin of kidney beans
+    1 vegetable stock cube ()
+    30g fresh coriander (optional)
+    1 lime
+    1 avocado
+    120g cheddar cheese
+    200g tortilla chips or corn chips, plain or lightly salted
+'''

3eacad7c7771b9c69cf27b048e6303c3bcddccf2
Ryan McClue <re.mcclue@protonmail.com>
2024-06-07 10:33:55 +1000

    j

---
+++ recipes/chickpea_curry.py
@@ -0,0 +1,70 @@
+from .common import *
+
+recipe = [
+  Ingredient("brown-onion", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("red-capsicum", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 6, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("lime", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("jasmine-rice", 350, INGREDIENT_CAT.OTHER),
+  Ingredient("cumin", 3, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("paprika", 1.5, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("chilli-powder", 1, INGREDIENT_CAT.CONDIMENT),
+  Ingredient("chicken-stock", 0.75, INGREDIENT_CAT.OTHER),
+  Ingredient("canned-tomatoes", 1.5, INGREDIENT_CAT.OTHER),
+  Ingredient("brown-sugar", 2.5, INGREDIENT_CAT.OTHER),
+  Ingredient("coconut-milk", 1.125, INGREDIENT_CAT.OTHER),
+  Ingredient("canned-chickpeas", 2.5, INGREDIENT_CAT.OTHER),
+  Ingredient("baby-spinach", 120, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+1. Dice onions. Slice capsicum (squash down).
+   Mince garlic cloves.
+2. Heat pot. Add washed rice and 550ml of cold water.
+   Add zest of 1 lime and pinch of salt.
+   Stir to break up any clumps.
+   Bring to a boil.
+   Then reduce heat to low, place on lid, and leave for 12minutes.
+   Take off heat, leaving lid on for 10minutes to steam.
+   Fluff rice with spatula.
+3. Sweat onions (cook with no colour) for 3minutes.
+   Sweat capsicum and garlic.
+   Add cumin, paprika, salt and chilli powder and toast for 1minute.
+   Add chicken stock to deglaze and make base sauce layer for 1minute.
+4. Add canned tomatoes, brown sugar and coconut milk.
+   Give big mix through and bring to heavy simmer.
+5. Add chickpeas and give big mix. 
+   Bring to boil and simmer for 20minutes
+   Add baby spinach for 1minute to wilt.
+   Add salt and pepper to taste.
+
+
+Ingredients - 
+2 Tbsp (40ml) - Olive Oil
+2 - Brown (Yellow) Onions, Diced 
+2 - Red Bell Peppers (Capsicums), Thinly Sliced 
+6 - Garlic Cloves, Minced 
+3 Tbsp (15g) - Ground Cumin 
+1 1/2 Tbsp (12g) - Paprika 
+1 tsp (2.5g) - Red Chilli Powder (Optional) 
+3/4 Cup (180ml) - Vegetable or Chicken Stock 
+1 1/2 Can (600g - 1.32lbs) - Diced Tomatoes
+2 1/2 Tbsp (35g) - Brown Sugar  
+1 1/4 Cans (500ml - 2 Cups) - Coconut Milk
+2 1/2 Cans (1kg - 2.2lbs) - Chickpeas, Drained 
+120g (4.23oz) - Baby Spinach, Washed 
+Seasoning To Taste 
+
+Jasmine Rice - 
+
+350g - Jasmine Rice, Washed 
+550ml (2 Cups + 2 1/2 Tbsp) - Cold Water 
+4 - Kaffir Lime Leave (Optional) - Can Be Substituted For The Zest of 1 Lime
+Salt To Taste 
+
+Garnish -
+
+50g - (1.76oz) - Natural Greek Yogurt 
+Lime Wedge To Serve 
+Coriander (Cilantro), To Serve 
+'''

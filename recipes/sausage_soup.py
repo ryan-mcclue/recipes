2a3e8e342dda59024e3dd91c86a8170866107c8a
Ryan McClue <re.mcclue@protonmail.com>
2024-06-16 12:39:33 +1000

    sa

--- recipes/sausage_soup.py
+++ recipes/sausage_soup.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: zlib-acknowledgement
+
+
+
+
+
 # italian sausage (casings removed)
 # https://www.youtube.com/watch?v=0XUJnWlbMUI
+'''
+1. Fry onion, carrot and celery.
+2. Add sausage with casings removed.
+3. Add chicken stock, potatoes and lentils.  
+4. Add thyme and bay leaf
+
+5. Cook tubeti pasta in separate pot
+   Serve pasta then top with broth.
+
+'''
+    10 cups Chicken Broth
+    1 1/2 cups French Green Lentils
+    1 lb. Hot Italian Pork Sausage, casings removed
+    3 Carrots, peeled and chopped
+    4 Celery Ribs, chopped
+    1 Small Yellow Onion, chopped
+    5 Golden Yukon Potatoes, cubed
+    4 Sprigs of Thyme
+    1 Bay Leaf
+    Handful of Parsley, chopped for topping
+    1 cup Tubeti
+    Extra Virgin Olive Oil
+    Salt, to taste

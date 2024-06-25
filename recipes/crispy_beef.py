2ba10c4d28da7114d6de01fc73d1dcafee930720
Ryan McClue <re.mcclue@protonmail.com>
2024-06-25 10:29:24 +1000

    u

---
+++ recipes/crispy_beef.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: zlib-acknowledgement
+
+'''
+preheat airfryer?
+
+airfryer requires spray oil?
+spray tray and spray contents
+
+school of wok
+
+
+roast garlic with skin on?
+'''

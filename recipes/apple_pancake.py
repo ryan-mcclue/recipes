8c656c38cb153175fe9df3b14c77b233a02e8ae3
Ryan McClue <re.mcclue@protonmail.com>
2024-06-28 12:49:50 +1000

    p

---
+++ recipes/apple_pancake.py
@@ -0,0 +1,56 @@
+from .common import *
+
+recipe = [
+  Ingredient("eggs", 2, INGREDIENT_CAT.DAIRY),
+  Ingredient("flour", 0.25, INGREDIENT_CAT.OTHER),
+  Ingredient("baking-powder", 0.25, INGREDIENT_CAT.OTHER),
+  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("milk", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("vanilla-extract", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("nutmeg", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("cinnamon", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("butter", 3, INGREDIENT_CAT.OTHER),
+  Ingredient("granny-smith-apple", 1, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+1. In a medium bowl, combine the eggs, flour, baking powder, sugar, and salt. 
+   Whisk lightly. 
+2. Gradually add milk in stages, whisking constantly as you do.
+3. Once all milk in, whisk in vanilla, melted butter, and nutmeg. 
+   Let stand for 30 minutes.
+4. Place the butter in a 6-inch round baking pan. 
+   Place the plan in the air fryer basket at 205C 2min.
+   When finished, brush some of the butter up the sides of the pan.
+5. Peel apple and chop into thin slices.
+6. In a small bowl, combine 2 tablespoons of sugar, cinnamon, and nutmeg. 
+   Stir until well combined.
+7. Sprinkle the spiced sugar mixture over the butter. 
+   Arrange apple slices in the pan in a single layer. 
+   Sprinkle the remaining 2 tablespoons sugar over the apples.
+   Set the air fryer to 2 minutes 205C. 
+8. Take out and pour the batter mixture over the apples. 
+   Set the air fryer to 177C and bake for 12 minutes.
+   Serve immediately
+
+
+Ingredients
+Batter
+2 Eggs
+1/4cup All Purpose Flour
+1/4tsp Baking Powder
+1 1/2tsp Sugar
+1 pinch Salt
+1/2cup Milk
+1/2tsp Vanilla Extract
+1/4tsp Ground Nutmug
+1tbsp butter, melted
+
+Apples
+1 Granny Smith apple 
+1/4cup Granulated Sugar
+1/4tsp Ground Cinnamon
+1/4tsp Ground Nutmeg
+2tbsp Butter
+
+'''

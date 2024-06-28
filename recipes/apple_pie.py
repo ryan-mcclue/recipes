8f7d85e7a2ca7e2538d469a48db795f6b1fc43ae
Ryan McClue <re.mcclue@protonmail.com>
2024-06-28 18:02:57 +1000

    p

---
+++ recipes/apple_pie.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: zlib-acknowledgement
+
+'''
+1. Add baking paper circle template and cut-out puff pastry
+2. Dice apple, 1tbsp sugar, 1/2tsp flour, dash of cinnamon/nutmeg
+3. Place fruit filling on half of circle leaving border.
+   Brush circle perimeter with water.
+   Fold over.
+   Crimp edges with fork.
+   Poke top with fork.
+4. Beat 1 egg and water and brush over pie.
+   Sprinkle with sugar.
+5. 20minutes at 160C
+'''

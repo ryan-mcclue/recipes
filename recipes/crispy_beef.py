7eb01fde3e417cd1e8c0a298aa37f2c7e9986826
Ryan McClue <re.mcclue@protonmail.com>
2024-06-29 10:47:24 +1000

    j

--- recipes/crispy_beef.py
+++ recipes/crispy_beef.py
@@ -2,13 +2,32 @@
 # SPDX-License-Identifier: zlib-acknowledgement
 
 '''
-preheat airfryer?
+brocolli, onion, capsicum, rump, garlic
 
-airfryer requires spray oil?
-spray tray and spray contents
+1. Roast 3 garlic cloves at 180C for 20min
+2. Mix beef with marinade.
+   Coat with cornflour seasoned with salt and pepper.
+   Ensure all beef strips are separated.
+3. Dice thumb ginger, roasted garlic, chilli and 20g coriander (stems and leaves)
+4. Add sauce ingredients in bowl
+5. Spray beef with some oil.
+   200C for 5 minutes.
+   Take out and spray again.
+   200C for another 5 minutes.
+6. Stir fry ginger, garlic, chilli breifly.
+   Pour over sauce and bring to boil.
+   Add meat.
+7. Top servings with coriander
 
-school of wok
+The Sauce
+½ tablespoon dark soy sauce
+3 tablespoons tomato ketchup
+6 tablespoons Chinkiang black rice vinegar
+4 tablespoons honey
 
-
-roast garlic with skin on?
+Marinade
+½ tablespoon sesame oil
+1 teaspoon sugar
+2 tablespoons light soy sauce
+1 egg
 '''

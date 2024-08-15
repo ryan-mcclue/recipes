6194b850934a8f2fdf6a83f650d0e36b8b7e37d1
Ryan McClue <re.mcclue@protonmail.com>
2024-08-15 19:24:13 +1000

    j

--- recipes/beef_noodle.py
+++ recipes/beef_noodle.py
@@ -1,7 +1,7 @@
 from .common import *
 
 recipe = [
-  Ingredient("chuck-steak", 800, INGREDIENT_CAT.MEAT),
+  Ingredient("chuck-steak", 1000, INGREDIENT_CAT.MEAT),
   Ingredient("chilli-bean-sauce-toban-djan", 1, INGREDIENT_CAT.OTHER),
   Ingredient("aromatic-spice-packet", 1, INGREDIENT_CAT.OTHER),
   Ingredient("tomato-paste", 1, INGREDIENT_CAT.OTHER),
@@ -18,16 +18,14 @@ recipe = [
   Ingredient("noodles", 400, INGREDIENT_CAT.OTHER),
 ] 
 '''
-1. Cube meat. Place in boiling water for 2-3minutes. 
-   Remove and set aside
-2. Meanwhile, roast spice bag spices for a few minutes.
-   Place in bag.
-3. When oil hot, add onion wedges, crushed garlic cloves, ginger slices and batoned spring onions.
-4. Add tomato wedges and meat.
-5. Add tomato paste, chilli bean paste, chinese wine, soy sauce, 1/2water-1/2beefstock.
-6. Add spice bag and 3 dates, scraping up bits off bottom of pan.
-7. Meat/stew setting for 60minutes.
-8. Strain broth.
+1. Heat oil and roast spices (and leaves) until fragrant.
+2. Add onion wedges, crushed garlic cloves, ginger slices and batoned spring onions.
+3. Add tomato wedges and meat.
+5. Add tomato paste, chilli bean paste, chinese wine, soy sauce, 1/2water-1/2beefstock and 3 dates.
+   Scrape up bits off bottom of pan.
+7. Meat/stew setting for 25 minutes. 
+   Natural release for 10
+8. Strain broth in pasta sieve. Then rice sieve to get spices out.
 9. Serve with noodles, bok choy, meat pieces and then add broth.
 
 Ingredients:
@@ -37,20 +35,17 @@ Ingredients:
 - 6 cloves garlic, crushes
 - 2 inch piece ginger, sliced
 - 4 scallions, chopped
-- 4 whole dried chillies
-- 1/2 cup soy sauce
+- 1/3 cup soy sauce
 - 1/2 cup shaoxing cooking wine
 - 1 tablespoon tomato paste
-- 2 tablespoons of douban jiang sauce (1 tablespoon is also fine)
+- 1 tablespoons of douban jiang sauce (this is strong; don't add too much)
 - 3 red dates 
 - 8 cups beef broth
-- 1 spice packet
   1 cinnamon stick
   2 bay leaves
-  1tbsp fennel seeds
-  1tbsp cumin seeds
-  1tbsp sichuan peppercorn
-  toast on pan for a few minutes. place in spice bag.
+  1tsp fennel seeds
+  1tsp cumin seeds
+  1tsp black peppercorn (sichuan peppercorn too star anisey?)
 
 Toppings for Garnish (to taste):
 - scallions
--- recipes/japanese_chicken.py
+++ recipes/japanese_chicken.py
@@ -1,10 +1,23 @@
 # SPDX-License-Identifier: zlib-acknowledgement
+from .common import *
+
+recipe = [
+  Ingredient("chicken-breast", 850, INGREDIENT_CAT.MEAT),
+  Ingredient("ginger", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("carrot", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("brown-onion", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("red-potato", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("coconut-milk", 125, INGREDIENT_CAT.OTHER),
+  Ingredient("vermont-curry-paste", 6, INGREDIENT_CAT.OTHER),
+  Ingredient("jasmine-rice", 400, INGREDIENT_CAT.OTHER),
+] 
 
 '''
 1. Saute ginger and garlic in oil.
 2. Add chicken, vegetables and water.
 3. Place curry paste blocks on top (to prevent scorching)
-4. High pressure for 4minutes
+4. High pressure for 7minutes
 5. Add coconut milk 
 
 Ingredients
@@ -12,12 +25,13 @@ Ingredients
     1 tablespoon Garlic
     1 tablespoon Minced Ginger
     1 pound Boneless Skinless Chicken Thighs boneless, skinless, cut into bite size pieces
-    1.5 cups Onion sliced thick
-    4 red potatoes quartered
-    2 Carrots chopped thick
+    1.5 cups Onion sliced thick (large onion)
+    4 red potatoes quartered (could add sweet potato)
+    2 Carrots chopped thick (large carrots)
     1.5 cups Water
     1/2 package Vermont Curry Paste 6 squares
     1/2 cup Full-Fat Coconut Milk
+    (could add nuts; maybe macadamias?)
 '''
 
 

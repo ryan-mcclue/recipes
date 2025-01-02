b1a01ca833db6d2ca46aa4e887ccad6420b46d0e
Ryan McClue <re.mcclue@protonmail.com>
2025-01-03 10:35:12 +1100

    p

---
+++ recipes/kra_pao.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("shallot", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("pork-mince", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("thai-red-chilli", 5, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("green-beans", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fish-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("oyster-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("dark-soy-sauce", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("palm-sugar/brown-sugar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("eggs", 4, INGREDIENT_CAT.DAIRY),
+  Ingredient("fresh-basil", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("peanut-oil", 0.25, INGREDIENT_CAT.OTHER),
+  Ingredient("jasmine-rice", 500, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+1. Combine 1tbsp light soy, 0.5tbsp dark soy, 1tbsp fish sauce, 1tbsp oyster sauce and sugar.
+2. In mortar, grind garlic, chillis and shallot into paste.
+3. Fry eggs separately in wok until bottom crispy.
+4. Cook mince with salt until caramelised (takes some time for moisture to leave and brown).
+   Remove
+5. Saute chilli paste for 1-2minutes.
+   Add cup of chopped beans for 1minute.
+   Add back meat.
+6. Add sauce and basil for 1minute.
+7. Serve with rice and egg on top.
+'''

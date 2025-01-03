7d046f02db5ca7403a963a9975cbfdaa81e3ad5a
Ryan McClue <re.mcclue@protonmail.com>
2025-01-03 16:37:56 +1100

    p

---
+++ recipes/black-pepper.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("rump-steak", 700, INGREDIENT_CAT.MEAT),
+  Ingredient("chinese-wine", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("cornstarch", 3, INGREDIENT_CAT.OTHER),
+  Ingredient("dark-soy-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("oyster-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("beef-stock", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("ketchup", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("black-pepper", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("sesame-oil", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sichuan-peppercorn", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("white-pepper", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("capsicum", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("brown-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 3, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("ginger", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("jasmine-rice", 500, INGREDIENT_CAT.OTHER),
+]
+
+'''
+1. Marinate beef slices with 2tbsp chinese wine, 1tsp soy, 1/4tsp salt, 1tbsp cornstarch.
+2. Combine 1/2cup beef stock, 1tbsp light souy, 1tsp dark soy, 1tbsp oyster, 1tbsp ketchup,
+   1tsp sugar, 2tbsp black pepper, 1/2tsp sichuan peppercorn, 1/2tsp white pepper, 2tsp cornstarch.
+3. Sear beef and remove.
+   Saute minced garlic, ginger and vegetables.
+4. Add back beef and sauce. Finish with sesame oil
+'''
---
+++ recipes/pad-see.py
@@ -0,0 +1,30 @@
+from .common import *
+
+recipe = [
+  Ingredient("peanut-oil", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("chicken-breast", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("soy-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("dark-soy-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("oyster-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("cornstarch", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("brocollini", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("eggs", 2, INGREDIENT_CAT.DAIRY),
+  Ingredient("dark-brown-sugar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("white-pepper", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("noodles", 500, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+1. Marinate chicken bits with 1tsp light soy, 1tsp oyster, 1tsp cornstarch.
+2. Combine 2tbsp light soy, 1.5tbsp dark soy, 2tbsp oyster, 2tbsp dark bron sugar, 1/2tsp white pepper
+3. Mince garlic.
+   Separate stem and heads of brocollini.
+4. Saute chicken and remove.
+5. Saute garlic with brocollini stems for 1-2minutes.
+   Then add heads.
+6. Move contents to side and crack 2 eggs, mixing to fry.
+   Remove.
+7. Add cooked noodles and sauce to mix.
+   Add back chicken and brocollini mixture.
+'''

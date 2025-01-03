0f12ca48b6a4c1f43847a9c9cabab8289c878cd0
Ryan McClue <re.mcclue@protonmail.com>
2025-01-03 11:09:38 +1100

    p

---
+++ recipes/mapo.py
@@ -0,0 +1,34 @@
+from .common import *
+
+recipe = [
+  Ingredient("pork-mince", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("ginger", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("spring-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("doubanjiang", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("chili-flakes", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("sichuan-peppercorn", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("chinese-wine", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("white-pepper", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("beef-stock", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("silken-tofu", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("corn-starch", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sesame-oil", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("jasmine-rice", 500, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+(toast sichuan peppercorns until fragrant than grind)
+1. Mince ginger and garlic. Slice spring onion.
+2. Simmer tofu cubes in salted water for 2-3minutes to firm up.
+3. Saute mince until caramelises (6-7 minutes)
+4. Add spring onion, ginger, garlic, 2tbsp doubanjiang, 1tsp chilli flakes,
+   and 1tsp peppercorn powder for 3-4minutes.
+5. Deglaze with 2tbsp chinese wine and simmer for 2-3minutes.
+   Add 1tsp soy sauce, 1tsp sugar and 1/2tsp white pepper.
+6. Add 2/3cup of stock and tofu and simmer.
+7. Mix 2tsp cornstarch and 3tbsp water to make slurry and add. 
+8. Finish with 1tsp sesame oil and green part of spring onions.
+'''

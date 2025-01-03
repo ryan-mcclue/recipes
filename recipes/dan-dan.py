b3316917bc2a152888ab9511eeb114642e446be0
Ryan McClue <re.mcclue@protonmail.com>
2025-01-03 12:49:49 +1100

    d

---
+++ recipes/dan-dan.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("peanut-oil", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("spring-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("beef-mince", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("ginger", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("garlic", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("pickled-mustard-greens", 0.25, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("chinese-wine", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("soy-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("dark-soy-sauce", 0.5, INGREDIENT_CAT.OTHER),
+  Ingredient("peanuts", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("bok-choy", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("sesame-paste", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("chinese-black-vinegar", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sesame-oil", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sichuan-chilli-oil", 5, INGREDIENT_CAT.OTHER),
+  Ingredient("noodles", 500, INGREDIENT_CAT.FRUIT_VEG),
+]
+
+'''
+1. Saute mince until caramelises.
+2. Add minced ginger, spring onion and 1/4cup pickled mustard greens.
+3. Deglaze with 2tbsp chinese wine and once evaporated, add 1tsp light and 1/2tsp dark soy and 1tsp sugar.
+   Set mince mixture aside.
+4. Combine 3tsp sesame paste, 2tbsp light soy, 1/2tsp dark soy, 1tbsp black vinegar, 1tsp sesame oil,
+   5tbsp chilli oil, 1tsp sugar, 2 garlic cloves grated.
+   Combine cooked noodles with sauce (can add some water if too thick).
+5. Serve mince mixture, noodles, bok choy, crushed peanuts and chilli oil
+'''

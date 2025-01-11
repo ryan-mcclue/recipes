9addd64af6b11ca79171f6a3d6ed30dbc08e5fc9
Ryan McClue <re.mcclue@protonmail.com>
2025-01-11 18:39:00 +1100

    meal prepping

---
+++ recipes/meal_prep.py
@@ -0,0 +1,43 @@
+from .common import *
+
+recipe = [
+  Ingredient("rice", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("black-beans", 2, INGREDIENT_CAT.OTHER),
+
+  Ingredient("shichimi-togarashi", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("fennel", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("old-bay", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("tajin", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("white-pepper", 2, INGREDIENT_CAT.OTHER),
+
+  Ingredient("lime", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("lemon", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("-flakes", 2, INGREDIENT_CAT.FRUIT_VEG),
+
+  Ingredient("barramundi", 2, INGREDIENT_CAT.MEAT),
+  Ingredient("chicken-breast", 2, INGREDIENT_CAT.MEAT),
+  Ingredient("beef-mince", 2, INGREDIENT_CAT.MEAT),
+  Ingredient("prawns", 2, INGREDIENT_CAT.MEAT),
+
+]
+
+'''
+1. Cook rice in rice cooker.
+   When fluffed, season with shichimi and juice and zest of 2 limes.
+(Put in container and let cool with date of cooked label and then in fridge.)
+2. Add black beans, salt, 3 bay leaves, 1 onion, 4 cloves of crushed garlic in presuure cooker.
+(Put in container and let cool with date of cooked label and then in fridge.)
+3. Cover fish with olive oil, salt, fennel, white pepper. 
+   Cook for 190 fan bake for 12minutes.
+(Put in container and let cool with date of cooked label and then in fridge.)
+4. Cover chicken with olive oil, old bay, salt.
+   Grill chicken till get bar lines then place in oven for 190.
+(Put in container and let cool with date of cooked label and then in fridge.)
+5. Cook mince in pan with salt and ras-el-hanout. 
+(Put in container and let cool with date of cooked label and then in fridge.)
+6. Cover peeled and deveined prawns with olive oil, salt, tajin.
+   Once prawn tails start to go pink, flip.
+   Turn heat off and add juice of a lemon.
+(Put in container and let cool with date of cooked label and then in fridge.)
+'''
+

30b25f31e74323ec90f3504da42f41ca69e4c3d1
Ryan McClue <re.mcclue@protonmail.com>
2025-01-11 20:28:30 +1100

    p

--- recipes/meal_prep.py
+++ recipes/meal_prep.py
@@ -8,11 +8,26 @@ recipe = [
   Ingredient("fennel", 2, INGREDIENT_CAT.OTHER),
   Ingredient("old-bay", 2, INGREDIENT_CAT.OTHER),
   Ingredient("tajin", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("coriander", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("lemon-myrtle", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("cumin", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("paprika", 2, INGREDIENT_CAT.OTHER),
   Ingredient("white-pepper", 2, INGREDIENT_CAT.OTHER),
+
+  Ingredient("sherry-vinegar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("apple-cider-vinegar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("coconut-vinegar", 2, INGREDIENT_CAT.OTHER),
 
   Ingredient("lime", 2, INGREDIENT_CAT.FRUIT_VEG),
   Ingredient("lemon", 1, INGREDIENT_CAT.FRUIT_VEG),
-  Ingredient("-flakes", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("zuchinni", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("capsicum", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("squash", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("corn", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("asparagus", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("baby-spinach", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("red-cabbage", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("cucumber", 1, INGREDIENT_CAT.FRUIT_VEG),
 
   Ingredient("barramundi", 2, INGREDIENT_CAT.MEAT),
   Ingredient("chicken-breast", 2, INGREDIENT_CAT.MEAT),
@@ -39,5 +54,26 @@ recipe = [
    Once prawn tails start to go pink, flip.
    Turn heat off and add juice of a lemon.
 (Put in container and let cool with date of cooked label and then in fridge.)
+(Put in container and let cool with date of cooked label and then in fridge.)
+7. Cut zuchinni halves diagonally (undercook veggies so not soggy for meal prep)
+   Fry with salt and cumin.
+   Turn heat and as heat lowering top with some sherry vinegar
+(Put in container and let cool with date of cooked label and then in fridge.)
+8. Cut squash halves into 3 pieces. 
+   Fry with salt and coriander.
+   Turn heat and as heat lowering top with some apple cider vinegar
+8. Cut capsicum into into strips. 
+   Fry with salt and paprika.
+   Turn heat and as heat lowering top with some lemon juice.
+9. Cut asparagus into into strips. 
+   Fry with salt.
+   Turn heat and as heat lowering top with some lemon myrtle.
+   Then top with coconut vinegar.
+10. Cut kernels off side of corn into a bowl
+   Fry with salt.
+11. Put baby spinach into container.
+12. Mix red cabbage slices with salt and coconut vinegar.
+13. Cut cumber into halves and remove seed part.
+    Then cut diagonally.
 '''
 

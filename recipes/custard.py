5d38a508cba09a4d425ddf4840cd20e9d2292e97
Ryan McClue <re.mcclue@protonmail.com>
2025-02-09 17:25:32 +1100

    Create custard.py

---
+++ recipes/custard.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("honey", 4, INGREDIENT_CAT.OTHER),
+  Ingredient("cinnamon-stick", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("vanilla-bean", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("orange", 2, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("coconut-milk", 400, INGREDIENT_CAT.OTHER),
+  Ingredient("coconut-cream", 100, INGREDIENT_CAT.OTHER),
+  Ingredient("vanilla-paste", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("cornflour", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("egg", 2, INGREDIENT_CAT.DAIRY),
+  Ingredient("cinnamon", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("walnuts", 50, INGREDIENT_CAT.OTHER),
+  Ingredient("sugar", 1, INGREDIENT_CAT.OTHER),
+]
+
+'''
+1. Add 400ml coconut milk, 100ml coconut cream, 1tsp vanilla paste and 2tbsp honey into saucepan.
+   Heat before boiling.
+2. In other bowl, whisk 2eggs, 1tbsp cornflour, pinch of cinnamon and salt.
+   While whisking add in milk/cream mixture.
+   Then add back to saucepan and heat until thickens. 
+3. Bake 0.5cup walnuts at 180deg for 8min.
+   Splash with water, cover with brown sugar and bake at 200deg for 8min.
+4. Add 2tbsp honey, 2tbsp water in very small saucepan.
+   Bring to boil then simmer for 3min.
+   Add 1 cinnamon stick, 0.5vanilla bean seeds and bean and 2 orange segments.
+   Combine for 2min.
+5. Serve milk/cream mixture, top with oranges and walnuts.
+'''

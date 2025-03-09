dfd890fc3834440a8aecbf875b91db0157122815
Ryan McClue <re.mcclue@protonmail.com>
2025-03-09 12:12:14 +1100

    Create creami.py

---
+++ creami.py
@@ -0,0 +1,35 @@
+"""
+Sorbet: 3 pears, skins removed, sliced.
+        whole canned peaches in juice.
+        Add pears and 3 peaches.
+        Fill halfway with peach juice.
+        Blend in with immersion blender.
+        Freeze for 24 hours.
+        
+
+freeze without lid on to avoid humps?
+IMPORTANT: freeze on level surface
+IMPORTANT: let container sit outside for at least 10 minutes
+IMPORTANT: if comes out powdery hit respin (can add some more milk)
+IMPORTANT: once done, the hole left can be used to mix-in 
+
+steep almond milk + sprigs of mint
+MILK + FLAVOURING 
+mint, vanilla, chocolate, 
+
+THICKENER 
+4 egg yolks whisked. Add 1/2cup sugar.
+Add 2 cups heavy cream to pan with 1 cup of almond milk.
+Heat slightly below simmering temperature.
+Add small amounts of mixture to eggs and whisk together till gets up to temperature.
+Then add back to pan on medium low heat and stir regularly for about 5 minutes.
+Cool down in ice-bath and pour into container, leaving room for flavouring.
+
+
+SWEETENER
+3 tbsp maple syrup 
+honey
+
+MIX-IN
+chocolate shavings
+"""

35261dbc871dfa75329b0195610843be04aaa17c
Ryan McClue <re.mcclue@protonmail.com>
2025-04-05 18:23:18 +1100

    Create roast_chicken.py

---
+++ recipes/roast_chicken.py
@@ -0,0 +1,60 @@
+from .common import *
+
+recipe = [
+  Ingredient("whole-chicken", 1, INGREDIENT_CAT.MEAT),
+  Ingredient("baby-capsicums", 300, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("dutch-carrots", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("baby-potato", 1000, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-thyme", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-chives", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fresh-tarragon", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("flour", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("chicken-stock", 1, INGREDIENT_CAT.OTHER),
+] 
+'''
+0. Preheat to 180deg.
+1. 
+    Place chicken on centre of tray.
+    Generously rub whole chicken with olive oil.
+    Put salt flakes all round.
+    Then top with pepper.
+    Place some of fresh thyme in chicken cavity.
+2.
+    Place whole baby peppers around edge of tray (other veges will top these as fastest to cook)
+    Slice salad potatoes in half and put around tray.
+    Cut carrots lengthwise and put around tray.
+    Drizzle all vegetables with olive oil.
+    Drizzle with salt.
+    Slice remaining bunch of fresh thyme and drizzle vegetables with it.
+3. 
+    Place in oven cavity facing outwards.
+4.
+    Check 15minutes. 
+    Most likely remove baby peppers as cooked.
+    Spread remaining vegetables out.
+5. 
+    Check 15 minutes and probably remove carrots.
+6. 
+    Check 15minutes and remove potatoes.
+7.
+    In another 10 minutes remove chicken.
+    Let rest for 5 minutes.
+    Before moving tip bird up and let cavity juices pour on pan.
+8.
+    Pour pan juices into a pot.
+    Pour 200ml chicken stock onto pan and remove hard bits.
+9.
+    Heat pot up and whisk in flour.
+    Cook flour out for 3-4minutes whisking regularly.
+    Pour remaining 300ml chicken stock and pan juices into pot still whisking.
+    Let simmer for 10minutes stirring often.
+10.
+    Place vegetables on tray and back in oven to keep warm.
+    Carve chicken (https://youtu.be/Vs_FDwBcUck?t=423)
+11.
+    Mix in 1/4bunch fresh chives on potatoes.
+    Top chicken and carrots+peppers with some fennel.
+12.
+    Chop 1/4bunch fresh taragon into a pitcher.
+    Fill with gravy.
+'''

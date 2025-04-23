be05d200bf34bede650d51a81fcf90f8a2e726bf
Ryan McClue <re.mcclue@protonmail.com>
2025-04-23 10:37:30 +1000

    Create tofu_tempah.py

---
+++ recipes/tofu_tempah.py
@@ -0,0 +1,18 @@
+'''
+EXTRA-FIRM-TOFU:
+1. Pat dry, then break apart tofu with hand into bowl (want craggly edges).
+2. Drizzle with soy sauce, mix around, drizzle some more.
+3. Repeat this with corn starch.
+4. Repeat with avocado oil (high smoke point oil)
+5. Cook on max heat for 12minutes in air fryer (or until crispy)
+
+TEMPEH:
+1. Slice tempeh into chunks.
+2. Heat pan with avocado oil.
+3. Place tempeh on pan and sprinkle with salt.
+4. Splash some water and cover with lid.
+5. 3-4minutes later, add some more water for 2minutes.
+6. Flip tempeh bits over. 
+   Add another splash of water and 2-3minutes later.
+   In pan, season with garlic powder and paprika.
+'''

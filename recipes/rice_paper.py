9d0ee80b7fb38e54f2b9ae3e92c96e5742ae3db8
Ryan McClue <re.mcclue@protonmail.com>
2025-07-28 13:26:59 +1000

    Create rice_paper.py

---
+++ recipes/rice_paper.py
@@ -0,0 +1,31 @@
+from .common import *
+
+recipe = [
+  Ingredient("chicken-breast", 500, INGREDIENT_CAT.MEAT),
+  Ingredient("garlic", 4, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("shallot", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("lemongrass", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("spring-onion", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("cucumber", 1, INGREDIENT_CAT.FRUIT_VEG),
+  Ingredient("fish-sauce", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("light-soy", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("brown-sugar", 2, INGREDIENT_CAT.OTHER),
+  Ingredient("vermicelli", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("rice-paper-sheets", 8, INGREDIENT_CAT.OTHER),
+  Ingredient("cucumber", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("smooth-peanut-butter", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("hoisin", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("lime-juice", 1, INGREDIENT_CAT.OTHER),
+  Ingredient("sriracha", 1, INGREDIENT_CAT.OTHER),
+]
+
+'''
+(toast sichuan peppercorns until fragrant than grind)
+1. Combine thingly sliced chicken, 1 rated lemongrass stick, 1 grated shallot, 2 garlic, 2tbsp fish sauce, 2tbsp light soy,
+   1tbsp brown sugar. Marinate for 30minutes.
+2. Cook vermicelli in boiling water. Drain and run under cold water (squeeze moisture out).
+3. Brown chicken
+4. Dip rice paper sheet in cold water until just wet all over, then place smooth side down.
+5. Whisk 1/3cup peanut butter. Add dash of fish sauce, 1/4cup hoisin, 1/2lime juice.
+  Add sriracha.
+'''

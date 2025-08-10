893439a2771b9ed142175dda8121e3828091c0e4
Ryan McClue <re.mcclue@protonmail.com>
2025-08-10 12:48:39 +1000

    Create chilli_garlic.py

---
+++ recipes/chilli_garlic.py
@@ -0,0 +1,6 @@
+'''
+1. Mince 4 garlic, ginger. Dice spring onion
+2. Mix 1tbsp black vinegar, 2tbsp soy, 2tbsp gocuchang, sugar and sesame seeds to taste.
+3. Fry 500g pork. Add garlic, ginger and onion.
+4. Mix in sauce. Mix in noodles. Serve with egg.
+'''

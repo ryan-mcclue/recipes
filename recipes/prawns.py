011293f398a64e35e10f63e95c9f0d67656a5753
Ryan McClue <re.mcclue@protonmail.com>
2025-08-15 15:07:52 +1000

    Create prawns.py

---
+++ recipes/prawns.py
@@ -0,0 +1,6 @@
+'''
+1. Fry 2 shallots, 4 garlic, 10 halved cherry tomatoes
+2. Add 6 squash, 1 heaped tsp bippi italian chilli, 1 heaped tsp capers.
+3. Add 10 prawns
+4. Add cooked pasta with some pasta water.
+'''

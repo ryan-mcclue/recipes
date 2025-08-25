2ce639f28b645b29105701284f5a056e888857cc
Ryan McClue <re.mcclue@protonmail.com>
2025-08-25 11:12:59 +1000

    Create beef_stew.py

---
+++ recipes/beef_stew.py
@@ -0,0 +1,7 @@
+'''
+1. Coat 750g chuck cubes with 2tbsp flour.
+2. Add seasoning (salt, pepper, 1tsp onion powder, garlic power, brown sugar, sweet paprika, 1/2tsp thyme, oregano, 2 bay leaves),
+   1tbsp tomato paste, 1 cup bouillon paste and mix. 
+   Add cubes and mix.
+3. Top with diced onion, carrot, celery and potato. Don't mix.
+'''

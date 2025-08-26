18fe303611e4fa93f10b493bbe545f5137c6a8fc
Ryan McClue <re.mcclue@protonmail.com>
2025-08-26 16:40:51 +1000

    Create sweet_sour.py

---
+++ recipes/sweet_sour.py
@@ -0,0 +1,13 @@
+'''
+1. Mix 10tbsp flour, salt, pepper, 1/2tsp garlic powder and 2tsp paprika.
+2. Whisk two eggs in bowl
+3. Dredge diced chicken 3tbsp cornflour, egg and flour
+   (use one hand for flour and other for egg, i.e. dry and wet)
+4. Fry chicken and remove
+5. Fry brown onion and 2 capsicums. 
+   Add minced garlic and ginger.
+6. Add 2/3cup ketchup, 6tbsp dark brown sugar, 2 tbsp malt vinegar,
+   475g canned pineapple chunks in juice.
+   Bring to boil and simmer for 5 minutes till thickens.
+7. Add back chicken
+'''

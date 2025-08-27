631104426d641381ab0d1446f9dfc5ad4e12826f
Ryan McClue <re.mcclue@protonmail.com>
2025-08-27 17:10:06 +1000

    Update beef_stew.py

--- recipes/beef_stew.py
+++ recipes/beef_stew.py
@@ -1,8 +1,10 @@
 '''
-1. Coat 750g chuck cubes with 2tbsp flour.
-2. Add seasoning (salt, pepper, 1tsp onion powder, garlic power, brown sugar, sweet paprika, 1/2tsp thyme, oregano, 2 bay leaves),
+1. Dice 750g chuck steak.
+2. Mix minced garlic, 2tbsp tomato paste, 1 diced brown onion, fresh parsley,
+   2tsp herbs de provence, 1tsp smoked paprika.
+3. 1 cup boullion paste.
+3. Add  Add seasoning (salt, pepper, 1tsp onion powder, garlic power, brown sugar, sweet paprika, 1/2tsp thyme, oregano, 2 bay leaves),
    1tbsp tomato paste, 1 cup bouillon paste and mix. 
    Add cubes and mix.
-3. Top with diced onion, carrot, celery and potato. Don't mix.
-4. High pressure 40min. natural release 15.
+4. High pressure 25min. natural release 10.
 '''

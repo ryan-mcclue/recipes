ef6e24829375841a79d57fa18e13157b02ada70d
Ryan McClue <re.mcclue@protonmail.com>
2025-09-01 12:33:11 +1000

    Create gyros.py

---
+++ recipes/gyros.py
@@ -0,0 +1,8 @@
+'''
+1. Rub oil, fresh oregano, fresh rosemary, lemon pepper onto cubed meat
+2. Coat pita bread with olive oil, dried oregano (optional lemon pepper)
+3. On skewer, meat cube, diced cherry tomato, onion.
+   When cooked, drizzle with lemon juice.
+4. Dice cherry tomato, red onion, feta (bedding)
+5. Serve tzatiki, bedding, meat skewer (slide off)
+'''

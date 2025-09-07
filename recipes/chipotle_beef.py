92a9570472d165634f94fed8cbaac746f8671fe6
Ryan McClue <re.mcclue@protonmail.com>
2025-09-07 15:38:16 +1000

    Create chipotle_beef.py

---
+++ recipes/chipotle_beef.py
@@ -0,0 +1,6 @@
+'''
+1. chuck steak, 1 onion, 1 capsicum, 4 cloves garlic, jar chipotle adobo, 1 tin toms, 1tbsp smoked paprika, jarred jalapenos
+   cook high 25 minutes, natural release 15
+2. add can beans
+3. serve with corn chips
+'''
